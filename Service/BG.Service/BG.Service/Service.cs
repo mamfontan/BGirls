571e93b5e4e9fba64bace02f4f0d67575caf3492
Miguel Angel Moreno <mamfontan@gmail.com>
2022-02-28 15:29:25 +0100

    Skeleton for windows service

---
+++ Service/BG.Service/BG.Service/Service.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BG.Service
+{
+    public partial class Service : ServiceBase
+    {
+        private DateTime _actualTime = DateTime.Now;
+
+        private string _urlBase = "https://www.kindgirls.com/photo-archive.php?s=02-2022";
+
+        private Timer _timer;
+
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timer = new Timer();
+            _timer.Interval = 1000 * 60 * 60;
+            _timer.Start();
+            _timer.Elapsed += OnTimedEvent;
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        #region Private methods
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            var strYear = _actualTime.Year.ToString("####");
+            var strMonth = _actualTime.Month.ToString("##");
+
+        }
+        #endregion
+    }
+}
---
+++ Service/BG.Service/BG.Service/model/Gallery.cs
@@ -0,0 +1,21 @@
+﻿namespace BG.Service.model
+{
+    internal class Gallery
+    {
+        public int Id { get; set; }
+
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public int Views { get; set; }
+
+        public float Rating { get; set; }
+
+        public string[] Pics { get; set; }
+    }
+}
