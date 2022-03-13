ec23beb3a4c94f578f9f42165b2a5ca177f82e45
Miguel Angel Moreno Fontán <mamfontan@gmail.com>
2022-03-14 00:41:26 +0100

    Added new skeleton gallery list

---
+++ Front/app/src/main/java/arousa/com/bgirls/WallpaperListActivity.java
@@ -0,0 +1,34 @@
+package arousa.com.bgirls;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import arousa.com.bgirls.databinding.ActivityWallpaperListBinding;
+
+public class WallpaperListActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityWallpaperListBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallpaper_list);
+    }
+}
